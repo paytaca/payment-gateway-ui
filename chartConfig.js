d491adc0360a147dab332c68abb00ac5917d3fa2
Danil Jeus Ampatin <djampatin@gmail.com>
2023-04-03 10:35:14 +0800

    Updated Chart To Yearly Sales

--- chartConfig.js
+++ chartConfig.js
@@ -2,7 +2,7 @@ export const data = {
     labels: ['2012','2013', '2014', '2015', '2016', '2017', '2018', '2019' ,'2020', '2021', '2022', '2023'],
     datasets: [
       {
-        label: 'Montly Sales',
+        label: 'Yearly Sales',
         backgroundColor: '#0036fa',
         data: [40, 39, 10, 40, 39, 80, 40, 80, 23, 100, 64, 20]
       }
