01acf9b4795585d1adc21c210e77ab2da74456e6
Danil Jeus Ampatin <djampatin@gmail.com>
2023-04-05 14:28:26 +0800

    Front end Development

--- pages/login.vue
+++ pages/login.vue
@@ -43,36 +43,5 @@
 </template>
   
 <script>
-    // import Notification from '~/components/notification'
 
-    // export default {
-    //     components: {
-    //         Notification,
-    //     },
-
-    //     data() {
-    //         return {
-    //         email: '',
-    //         password: '',
-    //         error: null
-    //         }
-    //     },
-
-    //     methods: {
-    //         async login() {
-    //         try {
-    //             await this.$auth.loginWith('local', {
-    //                 data: {
-    //                 email: this.email,
-    //                 password: this.password
-    //                 }
-    //                 })
-
-    //                 this.$router.push('/')
-    //             } catch (e) {
-    //                 this.error = e.response.data.message
-    //             }
-    //         }
-    //     }
-    // }
 </script>
