8e59406300216a77e6bdf39149ba22b248ba0184
Danil Jeus Ampatin <djampatin@gmail.com>
2023-05-08 13:47:33 +0800

    Pie Chart Datas

--- components/appPiechart.vue
+++ components/appPiechart.vue
@@ -1,22 +1,88 @@
 <template>
-    <Pie :data="data" :options="options" />
-  </template>
-  
-  <script lang="ts">
-  import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-  import { Pie } from 'vue-chartjs'
-  import * as chartConfig from '~/piechartConfig.js'
-  
-  ChartJS.register(ArcElement, Tooltip, Legend)
-  
-  export default {
-    name: 'appPiechart',
-    components: {
-      Pie
-    },
-    data() {
-      return chartConfig
+  <Pie v-if="loaded" :data="chartData" :options="chartOptions"/>
+</template>
+
+<script>
+import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, LinearScale } from 'chart.js'
+import axios from "axios";
+
+ChartJS.register(Title, Tooltip, Legend, LinearScale)
+
+export default {
+  name: 'PieChart',
+  components: { Pie },
+  data() {
+    return {
+      loaded: false,
+      chartData: null,
+      chartOptions: {
+        maintainAspectRatio: false,
+
+      }
+    }
+  },
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const response = await axios.get('http://192.168.1.6:7878/payment-gateway/total-sales/', { mode: "no-cors"})
+      const data = response.data.map(item => [item.total_sale, item.total_orders, item.total_customers, item.products_sold]) 
+
+      this.chartData = {
+        labels: [
+          'Total Sales',
+          'Total Orders',
+          'Total Customers',
+          'Products Sold',
+        ],
+        datasets: [
+          {
+            backgroundColor: [
+              '#0036fa',
+              '#00d0ff',
+              '#00ff51',
+              '#ffb400',
+            ],
+            fill: false,
+            data: data[0],
+          },
+          {
+            backgroundColor: [
+              '#0036fa',
+              '#00d0ff',
+              '#00ff51',
+              '#ffb400',
+            ],
+            fill: false,
+            data: data[1],
+          },
+          {
+            backgroundColor: [
+              '#0036fa',
+              '#00d0ff',
+              '#00ff51',
+              '#ffb400',
+            ],
+            fill: false,
+            data: data[2],
+          },
+          {
+            backgroundColor: [
+              '#0036fa',
+              '#00d0ff',
+              '#00ff51',
+              '#ffb400',
+            ],
+            fill: false,
+            data: data[3],
+          },
+        ],
+      }
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
     }
   }
-  </script>
-  
+}
+</script>
