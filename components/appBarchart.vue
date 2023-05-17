70261c7c3b5c524a9c9a9675e877616dedb34d6d
Danil Jeus Ampatin <djampatin@gmail.com>
2023-05-17 16:40:12 +0800

    Bar Chart Fetching

--- components/appBarchart.vue
+++ components/appBarchart.vue
@@ -1,58 +1,85 @@
 <template>
-    <Bar v-if="loaded" :data="chartData" />
-  </template>
-  
-  <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  import axios from "axios";
-  
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-  
-  export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-      return {
-        loaded: false,
-        chartData: null
-      }
-    },
-    async mounted () {
-      this.loaded = false
+  <Bar v-if="loaded" :data="chartData" />
+</template>
 
-      try {
-        const response = await axios.get('http://192.168.1.10:7878/payment-gateway/total-sales-month/', { mode: "no-cors"})
-        const month = response.data.map(item => item.total_sale)
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import axios from "axios";
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      loaded: false,
+      chartData: null
+    }
+  },
+  async mounted() {
+  this.loaded = false
+  const store = localStorage.store_url;
+  console.log(store, 'monthly before')
+  try {
+    const response = await fetch('http://192.168.1.10:7878/payment-gateway/total-sales-month/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        store_url: store,
+      }),
+    })
+
+    if (response.ok) {
+      const responseData = await response.json()
+      console.log('Response Data:', responseData)
+
+      // Initialize labels and salesData arrays with default values
+      const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+      ];
+      const salesData = Array(12).fill(0);
+
+      // Update values based on responseData
+      responseData.forEach(item => {
+        const month = Number(item.month.substring(5));
+        salesData[month - 1] = parseFloat(item.total_sale);
+      });
 
         this.chartData = {
-          labels: [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December',
-          ],
+          labels: labels,
           datasets: [
             {
               label: 'Monthly Sales',
               backgroundColor: '#05fa4e',
-              data: month,
+              data: salesData,
             },
           ],
         }
-        this.loaded = true
-      } catch (e) {
-        console.error(e)
-      }
-    }
+
+        console.log('Chart Data:', this.chartData);
+      } else {
+        console.error('Failed to fetch chart data')
+      } 
+  } catch (error) {
+    console.error('An error occurred while fetching sales data:', error)
   }
-  </script>
-  
+
+  this.loaded = true
+}
+}
+</script>
