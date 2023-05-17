ece1d89acfa5d8d782c36e178daf00ec3544bcad
Danil Jeus Ampatin <djampatin@gmail.com>
2023-05-17 15:03:12 +0800

    Pie Chart Filtered

--- components/appPiechart.vue
+++ components/appPiechart.vue
@@ -24,65 +24,63 @@ export default {
   },
   async mounted () {
     this.loaded = false
+    const store = localStorage.store_url;
+    console.log(store, 'yesterday before')
 
     try {
-      const response = await axios.get('http://192.168.1.10:7878/payment-gateway/total-sales-yesterday/', { mode: "no-cors"})
-      const data = response.data.map(item => [item.total_sale, item.total_orders, item.total_customers, item.products_sold]) 
+      const response = await fetch('http://192.168.1.10:7878/payment-gateway/total-sales/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          store_url: store,
+        }),
+      })
 
-      this.chartData = {
-        labels: [
-          'Total Sales',
-          'Total Orders',
-          'Total Customers',
-          'Products Sold',
-        ],
-        datasets: [
-          {
-            backgroundColor: [
-              '#0036fa',
-              '#00d0ff',
-              '#00ff51',
-              '#ffb400',
+      if (response.ok) {
+        const responseData = await response.json()
+        console.log('Response Data yesterday:', responseData)
+
+        if (typeof responseData === 'object' && Object.keys(responseData).length > 0) {
+          this.chartData = {
+            labels: [
+              'Total Sales',
+              'Total Orders',
+              'Total Customers',
+              'Products Sold',
             ],
-            fill: false,
-            data: data[0],
-          },
-          {
-            backgroundColor: [
-              '#0036fa',
-              '#00d0ff',
-              '#00ff51',
-              '#ffb400',
+            datasets: [
+              {
+                backgroundColor: [
+                  '#0036fa',
+                  '#00d0ff',
+                  '#00ff51',
+                  '#ffb400',
+                ],
+                fill: false,
+                data: [
+                  responseData.total_sale,
+                  responseData.total_orders,
+                  responseData.total_customers,
+                  responseData.products_sold,
+                ],
+              },
             ],
-            fill: false,
-            data: data[1],
-          },
-          {
-            backgroundColor: [
-              '#0036fa',
-              '#00d0ff',
-              '#00ff51',
-              '#ffb400',
-            ],
-            fill: false,
-            data: data[2],
-          },
-          {
-            backgroundColor: [
-              '#0036fa',
-              '#00d0ff',
-              '#00ff51',
-              '#ffb400',
-            ],
-            fill: false,
-            data: data[3],
-          },
-        ],
+          };
+          // this.chartData = responseData;
+          console.log('Chart Data:', this.chartData);
+          // console.log('Sales Object:', this.sales); // Log the sales object
+        } else {
+          this.chartData = null;
+        }
+      } else {
+        console.error('Failed to fetch chart data')
       }
+    } catch (error) {
+      console.error('An error occurred while fetching sales data:', error)
+    }
       this.loaded = true
-    } catch (e) {
-      console.error(e)
     }
   }
-}
 </script>
